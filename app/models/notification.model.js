eae4d7b70330d606e8a4e43d6e79a642c41ebe48
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-06 07:59:20 +0100

    add 'read' property to schema for notifications that have been read

--- app/models/notification.model.js
+++ app/models/notification.model.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var NotificationSchema = new Schema({
     subject: String,
     content: String,
-    date: Date
+    date: Date,
+    read: Boolean
 });
 
 module.exports = mongoose.model('Notification', NotificationSchema);
