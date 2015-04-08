06350f1c9098ac2db268eacf23371cd70172f323
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-08 18:00:04 +0100

    configure enviroment port variable

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-var config = require('./app/config/config')
+var config = require('./config/config')
     mongoose = require('mongoose');
     
 mongoose.connect(config.db[process.env.NODE_ENV].uri, config.db[process.env.NODE_ENV].options, function (err) {
-  if (err) {
-    console.error('Could not connect to MongoDB.');
-    console.log(err);
-  }
+  if (err)
+    console.error('Could not connect to MongoDB. /n' + err);
+    
 });
 
 
 var app = require('./app/config/express');
 
 
-app.listen(process.env.NODE_ENV, function (){
+app.listen(config.port, function (){
   console.log("something is working...");
 });
