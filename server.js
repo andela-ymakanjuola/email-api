711161dc8703045bdfdeb873c7033d43f5fb667e
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-08 11:05:33 +0100

    refactor code

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+   
+var Notification = require('./app/models/notification.model');
+var router = require('./app/routes/notification.route');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect('mongodb://localhost/notification-db');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+app.use('/notifications', router);
+
+app.listen(3000, function (){
+  console.log("something is working...");
+});
