fe1460b828f4dd48db9df3bb0150d16b3ba8582d
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-08 16:10:15 +0100

    configure mongoose to use environmental variable

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 var config = require('./app/config/config')
     mongoose = require('mongoose');
     
-
-mongoose.connect('mongodb://andela-yinka:andela123@ds029541.mongolab.com:29541/notifications-db');
-
 mongoose.connect(config.db[process.env.NODE_ENV].uri, config.db[process.env.NODE_ENV].options, function (err) {
   if (err) {
     console.error('Could not connect to MongoDB.');
     console.log(err);
   }
 });
-
 
 
 var app = require('./app/config/express');
