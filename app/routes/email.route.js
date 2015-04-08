760c347cae80367404bd8b2fc8e8aa553b39ce7e
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-08 12:31:03 +0100

    create email route

---
+++ app/routes/email.route.js
@@ -0,0 +1,24 @@
+// Paste your API key here...
+var POSTMARK_KEY = "afd3d32c-0187-4b92-91e2-751fa3e2e96c";
+
+var postmark = require("postmark")(POSTMARK_KEY);
+
+
+
+module.exports = {
+  sendMail: function(notification) {
+    postmark.send({
+      "From": "yinka.makanjuola@andela.co", 
+      "To": "yinka.makanjuola@andela.co", 
+      "Subject": notification.subject, 
+      "TextBody": notification.content
+      }, function (err, to) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log("Email sent to: %s", to);
+    });
+  }
+
+};
