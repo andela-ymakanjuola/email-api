2229fff0869ce499ceafecbabfb61272ced05983
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-10 17:28:41 +0100

    add required property to subject, content and date

--- app/notification/models/notification.model.js
+++ app/notification/models/notification.model.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var NotificationSchema = new Schema({
-    subject: String,
-    content: String,
-    date: Date,
+    subject: {type: String, required},
+    content: {type: String, required},
+    date: {type: Date, required},
     read: Boolean,
     sent: Boolean 
 });
