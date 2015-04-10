a42542b54d97d9dfea037731d09367e47b4009df
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-10 17:39:35 +0100

    set required property to true

--- app/notification/models/notification.model.js
+++ app/notification/models/notification.model.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var NotificationSchema = new Schema({
-    subject: {type: String, required},
-    content: {type: String, required},
-    date: {type: Date, required},
+    subject: {type: String, required: true},
+    content: {type: String, required: true},
+    date: {type: Date, required: true},
     read: Boolean,
     sent: Boolean 
 });
