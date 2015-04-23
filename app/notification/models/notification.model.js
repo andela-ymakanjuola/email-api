53a74ea7c123be16734bbd048ef4e9264c6d92de
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-23 15:40:51 +0100

    add author property to schema to tie notifications to users

--- app/notification/models/notification.model.js
+++ app/notification/models/notification.model.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var NotificationSchema = new Schema({
+    author: {type: String, required: true},
     subject: {type: String, required: true},
     content: {type: String, required: true},
     date: {type: Date, required: true},
