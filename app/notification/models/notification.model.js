131c43f439aa6f89c7442a80bf5140c2830642ad
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-23 17:33:07 +0100

    add email to schema

--- app/notification/models/notification.model.js
+++ app/notification/models/notification.model.js
@@ -3,6 +3,7 @@ var Schema = mongoose.Schema;
 
 var NotificationSchema = new Schema({
     author: {type: String, required: true},
+    email: {type: String, required: true},
     subject: {type: String, required: true},
     content: {type: String, required: true},
     date: {type: Date, required: true},
